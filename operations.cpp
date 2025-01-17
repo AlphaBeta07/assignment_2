bafa09fd1821487b21d62491bd5aec6f49b97405
AlphaBeta07 <152062300+AlphaBeta07@users.noreply.github.com>
2025-01-17 22:26:14 +0530

    Add files via upload

---
+++ operations.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"enter any two numbers:";
+	cin>>num1>>num2;
+    //addition
+    cout<<"addition:";
+    cout<<(num1+num2)<<endl;
+    //subtraction
+    cout<<"subtraction:";
+    cout<<(num1-num2)<<endl;        
+    //multiplication
+    cout<<"multiplaction:";
+    cout<<(num1*num2)<<endl;    
+    //division
+    cout<<"division:";
+    cout<<(num1/num2)<<endl;    
+    //modulus
+    cout<<"modulus:";
+    cout<<(num1%num2)<<endl;    
+    //increament
+    cout<<"increament:";
+    cout<<(++num1)<<endl;    
+    //decreament
+    cout<<"decreament:";
+    cout<<(--num2);
+}
